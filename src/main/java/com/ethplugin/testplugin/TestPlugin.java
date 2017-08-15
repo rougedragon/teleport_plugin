bbde0d05e48b61a6ad8d9b7c637773a2a78d5e99
Lior <pertoul@gmail.com>
2017-08-15 15:08:02 +0200

    Implements remove tp position and add permissions

--- src/main/java/com/ethplugin/testplugin/TestPlugin.java
+++ src/main/java/com/ethplugin/testplugin/TestPlugin.java
@@ -44,9 +44,6 @@ public class TestPlugin extends JavaPlugin {
     	}
     	else if (cmd.getName().equalsIgnoreCase("settpposition")) { // Si c'est la commande "setposition" qui a été tapée:
     		getLogger().info("Commande settpposition recue.");
-    		if (args.length > 0) {
-    			getLogger().info("Recu argument : " + args[0]);
-    		}
     	    if(sender instanceof Player) {
 	    		// C'est un joueur qui a effectué la commande
     	        Player p = (Player)sender;// On récupère le joueur.
@@ -54,7 +51,6 @@ public class TestPlugin extends JavaPlugin {
         	        Location newPosition = p.getLocation();
         	        positionForTP = newPosition;
 
-        	        // TODO
         	        String name = args[0];
     	        	TpPoint tpPoint = TpPoint.findTpPointInList(tpPoints, name, p);
     	        	if (tpPoint != null) {
@@ -103,18 +99,45 @@ public class TestPlugin extends JavaPlugin {
     	    		p.sendMessage("Vous devez spécifier le nom du point. Exemple /settpposition maison");
         	        getLogger().info("Le joueur n'a pas mis de nom de tpPoint."); 
     	    	}
-/*    	        if (positionForTP != null) {
-        	        p.teleport(positionForTP);
-        	        getLogger().info("Le joueur a ete teleporte.");    	            	        	
-    	        }
-    	        else {
-        	        getLogger().info("Vous n'avez pas sauvegarde de position. Utilisez /settpposition.");    	            	        	    	        	
-    	        }*/
     	    } else {
     	        // C'est la console du serveur qui a effectuée la commande.
         		getLogger().info("La commande a ete envoyee depuis la console du serveur.");    	        
     	    }
     		return true;//On renvoie "true" pour dire que la commande était valide
+    	}
+    	else if (cmd.getName().equalsIgnoreCase("removetpposition")) { // Si c'est la commande "removetpposition" qui a été tapée:
+    		getLogger().info("Commande removetpposition recue.");
+    	    if(sender instanceof Player) {
+	    		// C'est un joueur qui a effectué la commande
+    	        Player p = (Player)sender;// On récupère le joueur.
+    	    	// Vérifie si le joueur a entré un argument
+    	        if (args.length > 0) {
+        	        String name = args[0];
+    	        	TpPoint tpPoint = TpPoint.findTpPointInList(tpPoints, name, p);
+    	        	if (tpPoint == null) {
+    	        		// Erreur, le point n'existe pas
+    	        		p.sendMessage("Ce point n'existe pas.");
+            	        getLogger().info("Le joueur veut supprimer un point qui n'existe pas."); 
+    	        	}
+    	        	else {
+    	        		// Supprime le point
+            	        tpPoints.remove(tpPoint);
+            	        
+            	        getLogger().info("Le point [" + args[0] + "] a ete supprime."); 
+            	        p.sendMessage("Le point [" + args[0] + "] a ete supprime.");     	        		
+    	        	}
+    	    	}
+    	    	else {
+    	    		p.sendMessage("Vous devez spécifier le nom du point. Exemple /removetppoint maison");
+        	        getLogger().info("Le joueur n'a pas mis de nom de tpPoint a supprimer."); 
+    	    	}
+    	           	        
+    	    } else {
+    	        // C'est la console du serveur qui a effectuée la commande.
+        		getLogger().info("La commande a ete envoyee depuis la console du serveur.");    	        
+    	    }
+    		return true;//On renvoie "true" pour dire que la commande était valide
+
     	}
 
     	return false;//Si une autre commande a été tapée on renvoie "false" pour dire qu'elle n'était pas valide.
